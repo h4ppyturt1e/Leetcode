4a528717c6c2c97fb0f6d90dc5ade8911a2eeb8b
h4ppyturt1e <ryan170402@gmail.com>
2023-05-09 22:49:30 +0700

    added odd cases in mid->mid and max->max

--- 135_Candy.py
+++ 135_Candy.py
@@ -1,10 +1,24 @@
 from typing import List
 
+class KVPair:
+    def __init__(self, index, val):
+        self.index = index
+        self.val = val
 
+    def __lt__(self, other):
+        return self.val < other.val
+
+    def __repr__(self):
+        return f"({self.key}, {self.val})" 
+
+    def __eq__(self, other) -> bool:
+        return self.val == other.val and self.index == other.index
+    
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         children = len(ratings)
         ratings_set = set(ratings)
+        print(ratings_set)
         
         # if all ratings are the same, return number of children
         num_stages = len(ratings_set)
@@ -44,65 +58,98 @@ class Solution:
                 cur = res_list[i]
                 # if first element
                 if i == 0:
-                    min_val, max_val = sorted([res_list[i], res_list[i+1]])
+                    min_res, max_res = sorted([res_list[i], res_list[i+1]])
                     min_flat, max_flat = sorted([flattened_list[i], flattened_list[i+1]])
                     # if cur is min or equal
-                    if cur == min_val or min_flat == max_flat:
+                    if cur == min_res or min_flat == max_flat:
                         res_list[i] = 1
                     # if cur is max
                     else:
-                        res_list[i] = min_val + 1
+                        res_list[i] = min_res + 1
                 
                 # if last element
                 elif i == len(res_list)-1:
-                    min_val, max_val = sorted([res_list[i], res_list[i-1]])
+                    min_res, max_res = sorted([res_list[i], res_list[i-1]])
                     min_flat, max_flat = sorted([flattened_list[i], flattened_list[i-1]])
                     # if cur is min or equal
-                    if cur == min_val or min_flat == max_flat:
+                    if cur == min_res or min_flat == max_flat:
                         res_list[i] = 1
                     # if cur is max
                     else:
-                        res_list[i] = min_val + 1
+                        res_list[i] = min_res + 1
                 
                 # sandwiched elements
                 else:
-                    res_triple = sorted([res_list[i-1], res_list[i], res_list[i+1]])
-                    flat_triple = sorted([flattened_list[i-1], flattened_list[i], flattened_list[i+1]])
+                    res_triple = sorted([KVPair(i-1, res_list[i-1]), KVPair(i, res_list[i]), KVPair(i+1, res_list[i+1])])
+                    flat_triple = sorted([KVPair(i-1, flattened_list[i-1]), KVPair(i, flattened_list[i]), KVPair(i+1, flattened_list[i+1])])
                     
-                    min_val, mid_val, max_val = res_triple
+                    min_res, mid_res, max_res = res_triple
                     min_flat, mid_flat, max_flat = flat_triple
                     
-                    if cur == min_flat: # includes case where all flats are equal
-                        print("     min")
-                        res_list[i] = 1 
-                        
-                    elif cur == mid_flat:
-                        print("     mid")
-                        res_list[i] = min_val + 1
-                        
-                        # case where flat values are equal and nothing was removed
-                        if mid_val == min_val and min_flat == mid_flat == max_flat:
-                            res_list[i] = min_val
-                        
-                        
-                        
-                    elif cur == max_flat and mid_flat != max_flat:
-                        print("     max")
-                        res_list[i] = mid_val + 1
+                    if res_triple == flat_triple:
+                        # no edits in past iterations
+                        if cur == min_res.val:
+                            res_list[i] = 1
+                        elif cur == mid_res.val:
+                            res_list[i] = min_res.val + 1
+                        else:
+                            res_list[i] = mid_res.val + 1
+                            
                     else:
-                        print("ERROR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                    
-                    
+                        # edits in past iterations
+                        # min
+                        if cur == min_res.val or min_flat.val == mid_flat.val == max_flat.val:
+                            res_list[i] = 1
+                            
+                        # mid
+                        elif cur == mid_res.val:                            
+                            # if previously min and now mid, set as min
+                            if min_flat.index == mid_flat.index:
+                                res_list[i] = min_res.val
+                                print("min->mid")
+                            
+                            # if previously mid and still mid, set as min+1
+                            elif mid_flat.index == mid_res.index:
+                                res_list[i] = min_res.val + 1
+                                print("mid->mid")
+                                
+                                # unless min and mid flats were originally equal
+                                # and min is now not max
+                                if min_flat.val == mid_flat.val and min_flat.index != max_res.index:
+                                    res_list[i] = min_res.val
+                                    print("odd case")
+
+                        # max
+                        elif cur == max_res.val:
+                            # if previously min and now max, set as 1
+                            if min_flat.index == max_res.index:
+                                res_list[i] = 1
+                                print("min->max")
+                            
+                            # if previously mid and now max, set as min+1
+                            elif mid_flat.index == max_res.index:
+                                res_list[i] = min_res.val + 1
+                                print("mid->max")
+                            
+                            # if previously max and still max, set as mid+1
+                            elif max_flat.index == max_res.index:
+                                res_list[i] = mid_res.val + 1
+                                print("max->max")
+                                
+                                # unless mid and max flats were originally equal
+                                # and mid is now not min
+                                if max_flat.val == mid_flat.val and mid_flat.index != min_res.index:
+                                    res_list[i] = min_res.val + 1
+                                    print("odd case")
+
                 print(f"sum: {sum(res_list)}, {res_list}, {i}")
-                    
-        
-        
+                            
         total = sum(res_list)
         # print(total)
         
         return total
         
-                 
+  
                 
 if __name__ == '__main__':
     ratings_list = [
@@ -113,7 +160,8 @@ if __name__ == '__main__':
         [1,2,2,2,5,3,3,2,2,5,1,3,3,3],
         [2,5,1,5,1,3,4,4,2,1,3,3,2,4,5],
         [1,1,1,2,2,2,3,3,3,4,4,4,5,5,5],
-        [1,2,87,87,87,2,1]
+        [1,2,87,87,87,2,1],
+        [29,51,87,87,72,12]
     ]
     sol = Solution()
     
