7e1a44a284f49903b30fa906907c03011ecbfc86
h4ppyturt1e <ryan170402@gmail.com>
2023-09-18 21:28:53 -0700

    got rid of unnecessary code

--- Two Pointers/42_TrapRainWater.py
+++ Two Pointers/42_TrapRainWater.py
@@ -1,4 +1,5 @@
 from typing import List
+from 
 
 class Solution:
     def trap(self, height: List[int]) -> int:
@@ -8,51 +9,29 @@ class Solution:
         leftMax = height[0]
         rightMax = height[-1]
         
-        leftWalls = []
-        rightWalls = []
+        wallsSet = set()
         
         for i in range(len(height)):
             curLeft = height[left+i]
             curRight = height[right-i]
             
             if curLeft >= leftMax:
-                leftWalls.append(left+i)
-                # leftWalls[left+i] = curLeft
+                wallsSet.add(left+i)
                 leftMax = curLeft
             
             if curRight >= rightMax:
-                rightWalls.append(right-i)
-                # rightWalls[right-i] = curRight
+                wallsSet.add(right-i)
                 rightMax = curRight
         
         total = 0
-        seen = set()
-        # print(f"leftWalls: {leftWalls}")
-        # print(f"rightWalls: {rightWalls}")
+        walls = sorted(list(wallsSet))
         
-        for j in range(len(leftWalls)-1):
-            a, b = leftWalls[j], leftWalls[j+1]
-            if (a,b) in seen: continue
+        for x in range(len(walls)-1):
+            a, b = walls[x], walls[x+1]
             diff = abs(a - b) - 1
             if diff < 1: continue
             rect = (min(height[a],height[b]) * diff)
             blocks = sum(height[a+1:b])
-            # print(f"a: {a}, b: {b}, diff: {diff}")
-            # print(f"rect: {rect}, blocks: {blocks}")
             total += (rect - blocks)
-            seen.update([(a,b)])
-            
-
-        for k in range(len(rightWalls)-1):
-            a, b = rightWalls[k], rightWalls[k+1]
-            if (b,a) in seen: continue
-            diff = abs(a - b) - 1
-            if diff < 1: continue
-            rect = (min(height[a],height[b]) * diff)
-            blocks = sum(height[b+1:a])
-            # print(f"a: {a}, b: {b}, diff: {diff}")
-            # print(f"rect: {rect}, blocks: {blocks}")
-            total += (rect - blocks)
-            seen.update([(b,a)])
         
         return total
